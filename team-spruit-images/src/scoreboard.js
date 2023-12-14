18f2cd6b9eb3c2e270375ff8f173c0aff4da3bca
Geordin Raganold <91495472+GRaganold@users.noreply.github.com>
2023-12-14 18:12:44 +0000

    fix 2

--- team-spruit-images/src/scoreboard.js
+++ team-spruit-images/src/scoreboard.js
@@ -307,9 +307,9 @@ export default function Results() {
 										fontFamily: "sans-serif",
 										WebkitTextStroke: `1.5px ${boxStyle.WebkitTextStrokeColor}`,
 									}}
-									// dangerouslySetInnerHTML={{
-									//	__html: formData.Opponent.replace("/", "<br />"),
-								//	}}
+									dangerouslySetInnerHTML={{
+									__html: formData.Opponent.replace("/", "<br />"),
+								}}
 								/></>)}
 								
 							</Box>
